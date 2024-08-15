3bdab0dad398446aaf5e6c006c987c2cfc5a5edb
Johnson Weniaru <123410446+JayJayWen@users.noreply.github.com>
2024-08-15 19:27:41 +0000

    updated Anniversary Invites

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,9 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+SELECT CustomerID FROM Customers
+WHERE EMAIL = 'atapley2j@kinetecoinc.com';
+
+INSERT INTO AnniversaryInvitees
+VALUES(92,3);
