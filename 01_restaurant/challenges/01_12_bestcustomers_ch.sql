4a5dd818e1a7e7f36ec428ca81ebfc8594a252fa
Johnson Weniaru <123410446+JayJayWen@users.noreply.github.com>
2024-08-16 22:56:25 +0000

    Selected best customers

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,11 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+
+SELECT Customers.FirstName, Customers.LastName, Customers.Email, COUNT(Orders.orderID) AS Number_of_Orders 
+FROM Customers
+JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY customers.CustomerID
+ORDER BY Number_of_Orders DESC
+LIMIT 10;
