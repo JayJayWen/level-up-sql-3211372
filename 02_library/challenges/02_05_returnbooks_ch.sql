d9b9dd2994c08d7e45e02e91928af6c6dd45962b
Johnson Weniaru <123410446+JayJayWen@users.noreply.github.com>
2024-08-17 19:07:11 +0000

    Returned books

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,25 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05'
+WHERE BookID = (
+    SELECT Loans.BookID 
+    FROM Loans
+    JOIN Books ON Loans.BookID = Books.BookID
+    WHERE Books.Barcode = '6435968624'
+)
+AND ReturnedDate IS NULL;
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05'
+WHERE BookID IN (
+    SELECT Loans.BookID 
+    FROM Loans
+    JOIN Books ON Loans.BookID = Books.BookID
+    WHERE Books.Barcode IN ('5677520613', '8730298424')
+)
+AND ReturnedDate IS NULL;
+
