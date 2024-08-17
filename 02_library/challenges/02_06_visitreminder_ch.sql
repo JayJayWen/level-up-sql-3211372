778dfd1e5aaede017b7887db7c6a7b94effff9ae
Johnson Weniaru <123410446+JayJayWen@users.noreply.github.com>
2024-08-17 19:43:05 +0000

    Patrons with few books loaned

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,18 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT COUNT(Loans.LoanID) AS no_of_Books, Patrons.FirstName, Patrons.LastName, Patrons.Email
+FROM Loans
+JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+GROUP BY Patrons.PatronID
+ORDER BY no_of_Books ASC
+
+
+-- To limit to the 10 patrons
+
+SELECT COUNT(Loans.LoanID) AS no_of_Books, Patrons.FirstName, Patrons.LastName, Patrons.Email
+FROM Loans
+JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+GROUP BY Patrons.PatronID
+ORDER BY no_of_Books ASC
+LIMIT 10;
