16b4b8a6fc51ad5679e2bcaca9650772b6e880eb
Johnson Weniaru <123410446+JayJayWen@users.noreply.github.com>
2024-08-17 16:45:23 +0000

    Copies of Dracula books available

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,17 @@
 -- Determine how many copies of the book 'Dracula'
+
+ SELECT COUNT(Books.Title) FROM Books
+    WHERE Books.Title = 'Dracula';
+
 -- are available for library patrons to borrow.
+
+SELECT(
+    (SELECT COUNT(Books.Title) FROM Books
+    WHERE Books.Title = 'Dracula') -
+
+    (SELECT COUNT(Books.Title) FROM Loans
+    JOIN Books ON Books.BookID = Loans.BookID
+    WHERE Books.Title = 'Dracula'
+    AND Loans.ReturnedDate IS NULL))
+  AS Number_of_books_available
+;
